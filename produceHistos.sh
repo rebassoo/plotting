870f1b82cb8599531da78e56c05d8aa720b728d6
Finn Rebassoo <finn.rebassoo@cern.ch>
2017-12-15 13:22:59 -0800

    Updated

--- produceHistos.sh
+++ produceHistos.sh
@@ -2,21 +2,21 @@
 
 #/hadoop/cms/store/user/rebassoo/
 
-#python histos_mue.py TestFiles SlimmedNtupler/Data/RunG
-python histos_mue.py ExclusiveWW SM_Madgraph/2017-11-17
-
-python histos_mue.py MuonEG crab_runBv3/171116_215828/0000
-python histos_mue.py MuonEG crab_runBv3/171116_215828/0001
-python histos_mue.py MuonEG crab_runC/171116_215845/0000
-python histos_mue.py MuonEG crab_runD/171116_215903/0000
-python histos_mue.py MuonEG crab_runE/171118_014838/0000
-python histos_mue.py MuonEG crab_runF/171118_014901/0000
-python histos_mue.py MuonEG crab_runG/171116_220001/0000
-python histos_mue.py MuonEG crab_runHv2/171116_220035/0000
-python histos_mue.py MuonEG crab_runHv3/171116_220053/0000
+python histos_mue.py latest MuonEG crab_runBv3
 
 
-python histos_mue.py DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_DY-50-up/171116_220210/0000
-python histos_mue.py WWTo2L2Nu_13TeV-powheg crab_WWTo2L2Nu_13TeV-powheg/171116_220227/0000
-python histos_mue.py TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/171116_220243/0000
-python histos_mue.py WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8 crab_WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/171116_220301/0000
+
+#python histos_mue.py MuonEG crab_runC/171211_194321/0000
+#python histos_mue.py MuonEG crab_runD/171116_215903/0000
+#python histos_mue.py MuonEG crab_runE/171118_014838/0000
+#python histos_mue.py MuonEG crab_runF/171118_014901/0000
+#python histos_mue.py MuonEG crab_runG/171211_194414/0000
+#python histos_mue.py MuonEG crab_runHv2/171116_220035/0000
+#python histos_mue.py MuonEG crab_runHv3/171116_220053/0000
+
+
+#python histos_mue.py DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_DY-50-up/171209_002508/0000
+#python histos_mue.py WWTo2L2Nu_13TeV-powheg crab_WWTo2L2Nu_13TeV-powheg/171211_194541/0000
+#python histos_mue.py TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/171209_002539/0000
+#python histos_mue.py WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8 crab_WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/171209_002552/0000
+#python histos_mue.py ExclusiveWW SM_Madgraph/2017-12-15
