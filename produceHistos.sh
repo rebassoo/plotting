f7fb0042cb7f4fcddc00d2f25b7550247d75ca3d
Finn Rebassoo <finn.rebassoo@cern.ch>
2017-12-22 16:53:21 -0800

    Updated

--- produceHistos.sh
+++ produceHistos.sh
@@ -6,12 +6,12 @@
 
 python histos_mue.py latest MuonEG crab_runBv3
 python histos_mue.py latest MuonEG crab_runC
-python histos_mue.py latest MuonEG crab_runD
-python histos_mue.py latest MuonEG crab_runE
-python histos_mue.py latest MuonEG crab_runF
+#python histos_mue.py latest MuonEG crab_runD
+#python histos_mue.py latest MuonEG crab_runE
+#python histos_mue.py latest MuonEG crab_runF
 python histos_mue.py latest MuonEG crab_runG
-python histos_mue.py latest MuonEG crab_runHv2
-python histos_mue.py latest MuonEG crab_runHv3
+#python histos_mue.py latest MuonEG crab_runHv2
+#python histos_mue.py latest MuonEG crab_runHv3
 
 python histos_mue.py latest DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_DY-50-up
 python histos_mue.py latest WWTo2L2Nu_13TeV-powheg crab_WWTo2L2Nu_13TeV-powheg
