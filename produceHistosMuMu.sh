22f04102dafa5a5d9603856a58156ea4f65a0a5d
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-05-07 10:26:10 -0700

    Updated for CMSSW_9_4_x

--- produceHistosMuMu.sh
+++ produceHistosMuMu.sh
@@ -2,13 +2,26 @@
 
 #/hadoop/cms/store/user/rebassoo/
 
+#2017 data
 
-python histos_mumu.py latest DoubleMuon crab_runBv3
+#python histos_mumu.py latest DoubleMuon crab_Run2017B
+#python histos_mumu.py specific DoubleMuon crab_Run2017C/180202_232539/0000
+#python histos_mumu.py specific DoubleMuon crab_Run2017C/180202_232539/0001
+python histos_mumu.py latest DoubleMuon crab_Run2017B
+python histos_mumu.py latest DoubleMuon crab_Run2017C
+python histos_mumu.py latest DoubleMuon crab_Run2017D
+#python histos_mumu.py latest DoubleMuon crab_Run2017D
+#python histos_mumu.py latest DoubleMuon crab_Run2017E
+#python histos_mumu.py latest DoubleMuon crab_Run2017F
+python histos_mumu.py latest DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8 crab_DY-50-up-94x-mumu
+#python histos_mumu.py latest DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 crab_DY-50-up-94x-Madgraph-ext1-mumu
+#python histos_mumu.py specific DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 crab_DY-50-up-94x-Madgraph-ext1-mumu/180202_232841/0000
+
+#python histos_mumu.py latest DoubleMuon crab_runBv3
 #Need to wait until get this job finished
-python histos_mumu.py latest DoubleMuon crab_runC
-python histos_mumu.py latest DoubleMuon crab_runG
-
-python histos_mumu.py latest DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_DY-50-up-mumu
+#python histos_mumu.py latest DoubleMuon crab_runC
+#python histos_mumu.py latest DoubleMuon crab_runG
+#python histos_mumu.py latest DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 crab_DY-50-up-mumu
 
 #MC
 #python histos_mue.py WWTo2L2Nu_13TeV-powheg crab_WWTo2L2Nu_13TeV-powheg/170428_214517/0000/
