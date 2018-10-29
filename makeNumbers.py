8dfe716651a89727c18d14835edba7e7e3071201
Finn Rebassoo <finn.rebassoo@cern.ch>
2018-10-29 14:55:38 -0700

    Updated

--- makeNumbers.py
+++ makeNumbers.py
@@ -31,7 +31,8 @@ MCsamples=[
 "QCD_Pt_470to600_TuneCP5_13TeV_pythia8",
 "QCD_Pt_600to800_TuneCP5_13TeV_pythia8",
 "QCD_Pt_800to1000_TuneCP5_13TeV_pythia8",
-"QCD_Pt_800to1000_TuneCP5_13TeV_pythia8"
+"QCD_Pt_800to1000_TuneCP5_13TeV_pythia8",
+"ExclusiveWW_a0w2p5e-6-SingleLepton-2017"
 ]
 
 CutflowHistoList=[
@@ -65,6 +66,7 @@ ttbar=[0]*numCuts
 SingleTop=[0]*numCuts
 Diboson=[0]*numCuts
 QCD=[0]*numCuts
+exclWWa02p5=[0]*numCuts
 total=[0]*numCuts
 
 Wjets_error=[0]*numCuts
@@ -72,6 +74,7 @@ ttbar_error=[0]*numCuts
 SingleTop_error=[0]*numCuts
 Diboson_error=[0]*numCuts
 QCD_error=[0]*numCuts
+exclWWa02p5_error=[0]*numCuts
 total_error=[0]*numCuts
 
 Wjets_all=[]
@@ -79,6 +82,7 @@ ttbar_all=[]
 SingleTop_all=[]
 Diboson_all=[]
 QCD_all=[]
+exclWWa02p5_all=[]
 total_all=[]
 data_all=[]
 
@@ -125,19 +129,23 @@ for cut in CutflowHistoList:
         if "QCD_" in sample:
             QCD[i]=integral+QCD[i]
             QCD_error[i]=m.sqrt(QCD_error[i]*QCD_error[i]+error*error)
+        if "ExclusiveWW_" in sample:
+            exclWWa02p5[i]=integral+exclWWa02p5[i]
+            exclWWa02p5_error[i]=m.sqrt(exclWWa02p5_error[i]*exclWWa02p5_error[i]+error*error)            
         it=it+1
     Wjets[i]=round(Wjets[i],1)
     ttbar[i]=round(ttbar[i],1)
     SingleTop[i]=round(SingleTop[i],1)
     Diboson[i]=round(Diboson[i],1)
     QCD[i]=round(QCD[i],1)
+    exclWWa02p5[i]=round(exclWWa02p5[i],1)
     total[i]=round(total[i],1)
     Wjets_error[i]=round(Wjets_error[i],1)
     ttbar_error[i]=round(ttbar_error[i],1)
     SingleTop_error[i]=round(SingleTop_error[i],1)
     Diboson_error[i]=round(Diboson_error[i],1)
     QCD_error[i]=round(QCD_error[i],1)
-
+    exclWWa02p5_error[i]=round(exclWWa02p5_error[i],1)
     total_error[i]=round(total_error[i],1)
     i=i+1
 
@@ -164,6 +172,7 @@ for at in CutflowHistoList:
     Diboson_all.append(str(Diboson[ait])+" +/- "+ str(Diboson_error[ait]))
     QCD_all.append(str(QCD[ait])+" +/- "+ str(QCD_error[ait]))
     total_all.append(str(total[ait])+" +/- "+ str(total_error[ait]))
+    exclWWa02p5_all.append(str(exclWWa02p5[ait])+" +/- "+ str(exclWWa02p5_error[ait]))
     if ait<6:
         data_all.append(str(data[ait]))
     ait=ait+1
@@ -184,8 +193,8 @@ print "Total MC:       ",total_all
 print "Data:           ",data_all
 
 
-data=[CutflowNameList,Wjets_all,ttbar_all,SingleTop_all,Diboson_all,QCD_all,total_all,data_all]
-data_names=["","Wjets:","ttbar:","SingleTop:","Diboson:","QCD:","total MC:","data:"]
+data=[CutflowNameList,Wjets_all,ttbar_all,SingleTop_all,Diboson_all,QCD_all,total_all,exclWWa02p5_all,data_all]
+data_names=["","Wjets:","ttbar:","SingleTop:","Diboson:","QCD:","total MC:","a0w=2.5e-6","data:"]
 
 dash='_'*200
 for i in range(len(data)):
@@ -193,9 +202,9 @@ for i in range(len(data)):
         print(dash)
         print('{:<20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}'.format(data_names[i],data[i][0],data[i][1],data[i][2],data[i][3],data[i][4],data[i][5],data[i][6],data[i][7],data[i][8]))
         print(dash)
-    if i == 7:
+    if i == 8:
         print('{:<20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}'.format(data_names[i],data[i][0],data[i][1],data[i][2],data[i][3],data[i][4],data[i][5]))
-    if i!=0 and i!=7:
+    if i!=0 and i!=8:
         print('{:<20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}{:^20s}'.format(data_names[i],data[i][0],data[i][1],data[i][2],data[i][3],data[i][4],data[i][5],data[i][6],data[i][7],data[i][8]))
 
 
