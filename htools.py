abdbb57947c0a717b3dfb830280412503e46eadf
Finn Rebassoo <finn.rebassoo@cern.ch>
2019-04-26 12:54:31 -0700

    Reorganized code to be much simpler to read

--- htools.py
+++ htools.py
@@ -3,6 +3,12 @@
 import math as m
 import random as r
 from ROOT import *
+import os
+import sys
+from os import listdir
+from os.path import isfile, join
+
+
 
 def GetDphi(phi1,phi2):
     result = phi1-phi2
@@ -11,28 +17,165 @@ def GetDphi(phi1,phi2):
     while result < -m.pi: result = 2*m.pi + result
     return result
 
-#def calcPixelProb0Track(sector,numI):
-#    if sector="45":
-#        prob=numI*numI*0.015-2.05*numI+93.
-#        return prob/100.
-#    if sector="56"
-#        prob=numI*numI*0.01625-2.1625*numI+92.625
-#        return prob/100.
-    
+def passPPSSimMixingSignal(numI):
+    #Sector45
+    prob45=100.-2.718*numI+0.0349*numI*numI-0.000187*numI*numI*numI
+    #Sector56
+    prob56=100.-2.892*numI+0.0387*numI*numI-0.000216*numI*numI*numI
 
-#def GetCrossingAngle(run,lumiblock):
-#    crossingAngle=0
-#    txtfile=''
-#    if run > 303718: txtfile='xangle_afterTS2_STABLEBEAMS_CLEANUP.csv'
-#    if run < 303718: txtfile='xangle_tillTS2_STABLEBEAMS_CLEANUP.csv'
-#    f = open(txtfile)
-#    for line in f:
-#        if float(line.split(' ')[0]) == run and float(line.split(' ')[2]) == lumiblock:
-#            crossingAngle=float(line.split(' ')[4])
-#            #if crossingAngle < 150:
-#            #    print crossingAngle
-#            break
-#    return crossingAngle
+    tot_prob=prob45*prob56
+    num1=r.random()
+    num2=r.random()
+    if num1 < prob45 and num2 < prob56:
+        return True
+    else:
+        return False
+
+def passPPSSimMixing():
+    passPPS=False
+    file_xi=TFile("xi.root")
+    #h_num_pix_45=file_xi.Get("h_numPixTracks_45")
+    #h_num_pix_56=file_xi.Get("h_numPixTracks_56")
+    #num_pix_45=h_num_pix_45.GetRandom()
+    #num_pix_56=h_num_pix_56.GetRandom()
+    num1=r.random()
+    num2=r.random()
+    #print "num1: ",num1
+    #print "num2: ",num2
+    #if num_pix_45 == 1 and num_pix_56 ==1:
+    if num1 < 0.292298 and num2 < 0.295310:
+        #h_xi_45=file_xi.Get("h_pixel_xi_45_1track")
+        h_xi_45=file_xi.Get("h_xi_23")
+        #h_xi_56=file_xi.Get("h_pixel_xi_56_1track")
+        h_xi_56=file_xi.Get("h_xi_123")
+        xi_45=h_xi_45.GetRandom()
+        xi_56=h_xi_56.GetRandom()
+        xi=[xi_45,xi_56]
+        #print "xi: ",xi
+        passPPS=True
+        return passPPS,xi
+    else:
+        xi=[]
+        return passPPS,xi
+
+def passPPSNewPixel(e,xi):
+    ii=0
+    for detId_rp in e.proton_rpid:
+        #pixel
+        if detId_rp == 23: 
+            xi["23"].append(e.proton_xi[ii])
+        #pixel
+        if detId_rp == 123: 
+            xi["123"].append(e.proton_xi[ii])
+        ii=ii+1
+    return True
+
+def passPPSNew(e,xi):
+    left=False
+    right=False
+    passesPPS=False
+    ii=0
+    #print "Run: ",e.run
+    #print "Lumiblock: ",e.lumiblock
+    run=e.run
+    lumiblock=e.lumiblock
+    for detId_rp in e.proton_rpid:
+        #pixel
+        if detId_rp == 23: 
+            xi["23"].append(e.proton_xi[ii])
+        #strips
+        if detId_rp == 3: 
+            xi["3"].append(e.proton_xi[ii])
+        #diamond
+        if detId_rp == 16: 
+            xi["16"].append(e.proton_xi[ii])
+        #pixel
+        if detId_rp == 123: 
+            xi["123"].append(e.proton_xi[ii])
+        #strips
+        if detId_rp == 103: 
+            xi["103"].append(e.proton_xi[ii])
+        #diamond
+        if detId_rp == 116: 
+            xi["116"].append(e.proton_xi[ii])
+        ii=ii+1
+
+    if len(xi["123"])==1 and len(xi["23"])==1:
+        passesPPS=True
+
+    return passesPPS
+
+
+def AddFilesToChain(chain,ListOfFiles,DATA):
+    i=0
+    num_events=0
+    for file in ListOfFiles:
+        i=i+1
+        if not DATA:
+            statinfo = os.stat(file)
+            if statinfo.st_size > 0:
+                chain.Add(file)
+                f=TFile(file)
+                h=TH1F()
+                h=f.Get("totalEvents/h_total_events")
+                num_events=num_events+h.GetEntries()
+        if DATA:
+            chain.Add(file)
+    print "Number of total files: ",i
+    print "Number of events: {0}".format(num_events)
+    #if i>5:
+    #    break
+
+
+def GetListOfFiles(sample_name,file_dir,DATA):
+    print "Is is Data: ",DATA
+    mypath_prefix='/hadoop/cms/store/user/rebassoo/'
+    #print os.listdir('/hadoop/cms/store/user/rebassoo/{0}/{1}'.format(sample_name,file_dir))
+    ListOfFiles=[]
+    if sys.argv[1] == 'latest':
+        if DATA: output_name=sample_name+'_'+file_dir.split('_')[1]
+        else: output_name=sample_name
+        m_date=0.
+        m_date_string=0.
+        m_time=0.
+        m_time_string=0.
+        for d in os.listdir('/hadoop/cms/store/user/rebassoo/{0}/{1}'.format(sample_name,file_dir)):
+            date=int(d.split('_')[0])
+            date_string=d.split('_')[0]
+            t=0
+            t_string=''
+            if '_' in d:
+                t=int(d.split('_')[1])
+                t_string=d.split('_')[1]
+            if date >= m_date: 
+                m_date=date
+                m_date_string=date_string
+                m_time=t
+                m_time_string=t_string
+                if t > m_time: 
+                    m_time=t
+                    m_time_string=t_string
+
+        print m_date
+        print m_time        
+        if m_time > 0:
+            sub_dir=str(m_date_string)+"_"+str(m_time_string)
+        else:
+            sub_dir=str(m_date_string)
+        print sub_dir
+        itt = 0
+        for i in os.listdir(mypath_prefix+'/{0}/{1}/{2}'.format(sample_name,file_dir,sub_dir)):
+            mypath=mypath_prefix+'{0}/{1}/{2}/{3}/'.format(sample_name,file_dir,sub_dir,i)
+            ListOfFiles += [mypath_prefix+'{0}/{1}/{2}/{3}/{4}'.format(sample_name,file_dir,sub_dir,i,f) for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    if sys.argv[1] == 'specific':
+        mypath=mypath_prefix+'{0}/{1}/'.format(sample_name,file_dir)
+        ListOfFiles = [mypath_prefix+'{0}/{1}/{2}'.format(sample_name,file_dir,f) for f in listdir(mypath) if isfile(join(mypath, f))]
+        output_name=sample_name+'_'+file_dir.split('_')[1].split('/')[0]+'_'+file_dir.split('/')[2]
+
+    return ListOfFiles,output_name
+
+
 
 def GetCrossingAngles():
     crossingAngles=dict()
@@ -117,97 +260,6 @@ def GetXi(x,y,pot,run,crossingAngle):
 
     return abs(xi)
 
-#def passPPSSimMixing(numInteractions):
-#def passPPSSimMixing(numInteractions):
-def passPPSSimMixingSignal(numI):
-    #Sector45
-    prob45=100.-2.718*numI+0.0349*numI*numI-0.000187*numI*numI*numI
-    #Sector56
-    prob56=100.-2.892*numI+0.0387*numI*numI-0.000216*numI*numI*numI
-
-    tot_prob=prob45*prob56
-    num1=r.random()
-    num2=r.random()
-    if num1 < prob45 and num2 < prob56:
-        return True
-    else:
-        return False
-
-def passPPSSimMixing():
-    passPPS=False
-
-    file_xi=TFile("xi.root")
-    #h_num_pix_45=file_xi.Get("h_numPixTracks_45")
-    #h_num_pix_56=file_xi.Get("h_numPixTracks_56")
-    #num_pix_45=h_num_pix_45.GetRandom()
-    #num_pix_56=h_num_pix_56.GetRandom()
-    num1=r.random()
-    num2=r.random()
-    #print "num1: ",num1
-    #print "num2: ",num2
-    #if num_pix_45 == 1 and num_pix_56 ==1:
-    if num1 < 0.292298 and num2 < 0.295310:
-        #h_xi_45=file_xi.Get("h_pixel_xi_45_1track")
-        h_xi_45=file_xi.Get("h_xi_23")
-        #h_xi_56=file_xi.Get("h_pixel_xi_56_1track")
-        h_xi_56=file_xi.Get("h_xi_123")
-        xi_45=h_xi_45.GetRandom()
-        xi_56=h_xi_56.GetRandom()
-        xi=[xi_45,xi_56]
-        #print "xi: ",xi
-        passPPS=True
-        return passPPS,xi
-    else:
-        xi=[]
-        return passPPS,xi
-
-def passPPSNewPixel(e,xi):
-    ii=0
-    for detId_rp in e.proton_rpid:
-        #pixel
-        if detId_rp == 23: 
-            xi["23"].append(e.proton_xi[ii])
-        #pixel
-        if detId_rp == 123: 
-            xi["123"].append(e.proton_xi[ii])
-        ii=ii+1
-    return True
-
-def passPPSNew(e,xi):
-    left=False
-    right=False
-    passesPPS=False
-    ii=0
-    #print "Run: ",e.run
-    #print "Lumiblock: ",e.lumiblock
-    run=e.run
-    lumiblock=e.lumiblock
-    for detId_rp in e.proton_rpid:
-        #pixel
-        if detId_rp == 23: 
-            xi["23"].append(e.proton_xi[ii])
-        #strips
-        if detId_rp == 3: 
-            xi["3"].append(e.proton_xi[ii])
-        #diamond
-        if detId_rp == 16: 
-            xi["16"].append(e.proton_xi[ii])
-        #pixel
-        if detId_rp == 123: 
-            xi["123"].append(e.proton_xi[ii])
-        #strips
-        if detId_rp == 103: 
-            xi["103"].append(e.proton_xi[ii])
-        #diamond
-        if detId_rp == 116: 
-            xi["116"].append(e.proton_xi[ii])
-        ii=ii+1
-
-    if len(xi["123"])==1 and len(xi["23"])==1:
-        passesPPS=True
-
-    return passesPPS
-
 
 def passPPS(e,xi,crossingAngle):
     left=False
@@ -269,5 +321,4 @@ def passPPS(e,xi,crossingAngle):
     #    #passesPPS=False
 
     return passesPPS
-
 
