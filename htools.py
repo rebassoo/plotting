919810ffd18bebcaef6ba27cd9d7270bc0332273
Finn Rebassoo <finn.rebassoo@cern.ch>
2019-05-09 08:43:23 -0700

    Updated

--- htools.py
+++ htools.py
@@ -153,7 +153,7 @@ def GetListOfFiles(sample_name,file_dir,DATA):
         m_date_string=0.
         m_time=0.
         m_time_string=0.
-        for d in os.listdir(mypath_prefix'/{0}/{1}'.format(sample_name,file_dir)):
+        for d in os.listdir(mypath_prefix+'/{0}/{1}'.format(sample_name,file_dir)):
             date=int(d.split('_')[0])
             date_string=d.split('_')[0]
             t=0
