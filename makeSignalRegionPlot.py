f40b0c6927787bf6723bb14bb740598b57045274
Finn Rebassoo <finn.rebassoo@cern.ch>
2019-09-06 09:54:24 -0700

    Updated

--- makeSignalRegionPlot.py
+++ makeSignalRegionPlot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python                                                                                   
 #Finn Rebassoo, LLNL 04-30-2019 
+import math as m
 from ptools import *
 from ROOT import *
 
 
-_file0 = TFile("histos_muon/SingleMuonTotal.root")
+
+directory="histos_electron"
+_file0 = TFile("{0}/SingleElectronTotal.root".format(directory))
 
 #h_MWW_extra_tracks_0_4_notPPS->GetEntries()
 #h_MWW_extra_tracks_9_up_notPPS->GetEntries()
@@ -19,14 +22,29 @@ deno=h_MWW_extra_tracks_5_15_notPPS.GetEntries()
 print "num: ",num
 print "deno: ",deno
 histo_name_control="h_MWW_MX_control_Ycut"
+#histo_name_control="h_MWW_MX_control"
 h1=_file0.Get(histo_name_control)
 h1.Rebin(10)
 print "h_MWW_MX_control.GetEntries(): ",h1.GetEntries()
 #h_MWW_MX_10_up.Scale(0.012282431)
 h1.Sumw2()
-h1.Scale(num/deno)
+nbins=h1.GetNbinsX()
+scale_factor=num/deno
+scale_factor_error=scale_factor*(m.sqrt(1/num+1/deno))
+for i in range(0,nbins+1):
+    entry=h1.GetBinContent(i)
+    entry_error=h1.GetBinError(i)
+    if scale_factor > 0 and entry > 0:
+        total_bin_error=m.sqrt( m.pow((scale_factor_error/scale_factor),2) + m.pow(entry_error/entry,2)  )
+    else:
+        total_bin_error=0
+    h1.SetBinContent(i,entry*scale_factor)
+    h1.SetBinError(i,entry*scale_factor*total_bin_error)
+    #h1.SetBinError(i,0)
+#h1.Scale(num/deno)
 h1.SetStats(0)
 h1.GetYaxis().SetRangeUser(0,5)
+print "Data total prediction: ",h1.Integral(0,1001) 
 h1.Draw("e")
 
 #h_MWW_MX_0_4_tracks_notPPS.Scale(0.098)
@@ -40,7 +58,7 @@ histo_name="h_MWW_MX_0_4_tracks_Ycut"
 
 
 #_file1 = TFile("histos_electron/ExclusiveWW_a0w1e-6-SingleLepton-2017.root")
-_file1 = TFile("histos_electron/GGToWW_bSM-A0W1e-6_13TeV-fpmc-herwig6.root")
+_file1 = TFile("{0}/GGToWW_bSM-A0W1e-6_13TeV-fpmc-herwig6.root".format(directory))
 ##This is luminosity factor
 h2=_file1.Get(histo_name)
 print h2.Integral(0,1001)
@@ -57,14 +75,15 @@ h2.Draw("histsame")
 #h2.Draw()
 
 
-#_file1 = TFile("histos_electron/GGToWWTo3L3Nu_PtL-20_13TeV-fpmc-herwig6.root")
-#h3=_file1.Get(histo_name)
-#ModifyHisto(h3,"GGToWW_bSM-A0W1e-6_13TeV-fpmc-herwig6")
-#h3.SetLineColor(6)
-#h3.SetFillColor(0)
-###h_MWW_MX_0_4_tracks->GetNbinsX()
-#h3.Rebin(10)
-#h3.SetLineColor(1)
-#h3.Draw("histsame")
-##h2.Draw()
+#_file2 = TFile("{0}/GGToWWToJJMuNu_PtL-15_13TeV-fpmc-herwig6.root".format(directory))
+_file2 = TFile("{0}/GGToWWToJJENu_PtL-15_13TeV-fpmc-herwig6.root".format(directory))
+#_file2 = TFile("histos_muon/ExclusiveWW_a0w1e-6-SingleLepton-2017.root")
+h3=_file2.Get(histo_name)
+#ModifyHisto(h3,"GGToWWToJJMuNu_PtL-15_13TeV-fpmc-herwig6")
+ModifyHisto(h3,"GGToWWToJJENu_PtL-15_13TeV-fpmc-herwig6")
+#ModifyHisto(h3,"ExclusiveWW_a0w1e-6-SingleLepton-2017")
+h3.SetFillColor(0)
+h3.Rebin(10)
+h3.SetLineColor(1)
+h3.Draw("histsame")
 
