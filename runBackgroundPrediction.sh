7856de196db86c1006ed0173f1284e59f2af8d4f
Finn Rebassoo <finn.rebassoo@cern.ch>
2020-01-21 13:05:09 -0800

    Updated

--- runBackgroundPrediction.sh
+++ runBackgroundPrediction.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
-python makeSignalRegionPlotGeneral.py 2019-11-05-Pixel-Pixel noYcut muon 5_up
-python makeSignalRegionPlotGeneral.py 2019-11-05-MultiRP noYcut muon 5_up
-python makeSignalRegionPlotGeneral.py 2019-11-08-MultiPixel noYcut muon 5_up
+#python makeSignalRegionPlotGeneral.py 2019-12-11-Electron-MultiRP noYcut electron template_5up
+#python makeSignalRegionPlotGeneral.py 2019-12-11-Electron-Pixel-Pixel noYcut electron template_5up
+#python makeSignalRegionPlotGeneral.py 2019-12-18-Electron-MultiPixel noYcut electron template_5up
 
-python makeSignalRegionPlotGeneral.py 2019-11-05-Pixel-Pixel noYcut muon template
-python makeSignalRegionPlotGeneral.py 2019-11-05-MultiRP noYcut muon template
-python makeSignalRegionPlotGeneral.py 2019-11-08-MultiPixel noYcut muon template
+#python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-MultiRP noYcut muon template_5up
+#python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-Pixel-Pixel noYcut muon template_5up
+#python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-MultiPixel noYcut muon template_5up
 
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-Pixel-Pixel noYcut electron 5_up
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiRP noYcut electron 5_up
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiPixel noYcut electron 5_up
+python makeSignalRegionPlotGeneral.py 2019-12-11-Electron-MultiRP Ycut electron template_5up
+python makeSignalRegionPlotGeneral.py 2019-12-11-Electron-Pixel-Pixel Ycut electron template_5up
+python makeSignalRegionPlotGeneral.py 2019-12-18-Electron-MultiPixel Ycut electron template_5up
 
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-Pixel-Pixel noYcut electron template
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiRP noYcut electron template
-python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiPixel noYcut electron template
+python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-MultiRP Ycut muon template_5up
+python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-Pixel-Pixel Ycut muon template_5up
+python makeSignalRegionPlotGeneral.py 2019-12-04-Muon-MultiPixel Ycut muon template_5up
+
+
+
+#python makeSignalRegionPlotGeneral.py 2019-11-19-Electron-MultiRP noYcut electron template_5up
+#python makeSignalRegionPlotGeneral.py 2019-11-19-Electron-Pixel-Pixel noYcut electron template_5up
+#python makeSignalRegionPlotGeneral.py 2019-11-19-Electron-MultiPixel noYcut electron template_5up
+
+#python makeSignalRegionPlotGeneral.py 2019-11-18-MultiRP noYcut muon template_5up
+#python makeSignalRegionPlotGeneral.py 2019-11-18-Pixel-Pixel noYcut muon template_5up
+#python makeSignalRegionPlotGeneral.py 2019-11-18-MultiPixel noYcut muon template_5up
+
+
+
+#python makeSignalRegionPlotGeneral.py 2019-11-05-Pixel-Pixel noYcut muon 5_up
+#python makeSignalRegionPlotGeneral.py 2019-11-05-MultiRP noYcut muon 5_up
+#python makeSignalRegionPlotGeneral.py 2019-11-08-MultiPixel noYcut muon 5_up
+
+#python makeSignalRegionPlotGeneral.py 2019-11-05-Pixel-Pixel noYcut muon template
+#python makeSignalRegionPlotGeneral.py 2019-11-05-MultiRP noYcut muon template
+#python makeSignalRegionPlotGeneral.py 2019-11-08-MultiPixel noYcut muon template
+
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-Pixel-Pixel noYcut electron 5_up
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiRP noYcut electron 5_up
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiPixel noYcut electron 5_up
+
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-Pixel-Pixel noYcut electron template
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiRP noYcut electron template
+#python makeSignalRegionPlotGeneral.py 2019-11-08-Electron-MultiPixel noYcut electron template
 
 
 
