95804b266af5c546db5fb20f5ab808b05889f3a0
Finn Rebassoo <finn.rebassoo@cern.ch>
2020-01-21 13:01:38 -0800

    Updated significantly

--- makeSignalRegionPlotGeneral.py
+++ makeSignalRegionPlotGeneral.py
@@ -8,8 +8,21 @@ from htools import *
 
 def main(direc,Ycut,channel,background_method):
 
+    backgroundMC=True
     rebin=5
-    maximum=5
+    maximum=4
+
+    fout=TFile("combined_shapes_{0}_{1}.root".format(channel,direc.split("-")[4]),"recreate")
+    fout.cd()
+
+    #signal_1e6=TH1F()
+    #signal_2e6=TH1F()
+    #signal_5e6=TH1F()
+    #background=TH1F()
+    #background_up=TH1F()
+    #background_down=TH1F()
+    #data_obs=TH1F()
+
 
     c=TCanvas("c","",800,800)
     c.cd()
@@ -22,20 +35,21 @@ def main(direc,Ycut,channel,background_method):
     if channel=="electron":
         _file0 = TFile("{0}/SingleElectronTotal.root".format(directory))
 
-
-
-    num=h_MWW_extra_tracks_0_4_notPPS.GetEntries()
+    num=_file0.Get("h_MWW_extra_tracks_0_4_notPPS").GetEntries()
+    print "Num of events in 0-4 extra tracks not PPS: ",num
     h1=TH1F()
-    if background_method=="5_up":
-        deno=h_MWW_extra_tracks_5_up_notPPS.GetEntries()
+    h0=TH1F()
+    if background_method=="5_up" or background_method=="template_5up":
+        deno=_file0.Get("h_MWW_extra_tracks_5_up_notPPS").GetEntries()
         histo_name_control="h_MWW_MX_5_up"
         if Ycut=="Ycut":     histo_name_control="h_MWW_MX_control_5_up_Ycut"
         h1=_file0.Get(histo_name_control)
         h1.Rebin(rebin)
-        print "h_MWW_MX_control.GetEntries(): ",h1.Integral(0,1001)
+        #print "h_MWW_MX_control.GetEntries(): ",h1.Integral(0,1001)
         h1.Sumw2()
         nbins=h1.GetNbinsX()
         scale_factor=num/deno
+        print "Scale factor for high to low extra tracks for ABCD is: ",scale_factor
         scale_factor_error=scale_factor*(m.sqrt(1/num+1/deno))
         for i in range(0,nbins+2):
             entry=h1.GetBinContent(i)
@@ -50,9 +64,11 @@ def main(direc,Ycut,channel,background_method):
         h1.GetYaxis().SetRangeUser(0,maximum)
         h1.GetXaxis().SetTitle("M_{WW}/M_{X}")
         h1.GetYaxis().SetTitle("Events")
-        print "Data total prediction: ",h1.Integral(0,11) 
+        print "Data total prediction: ",h1.Integral(0,1001) 
         h1.Draw("e")
-
+        h0=h1.Clone()
+        h0.SetLineColor(2)
+        h0.SetLineWidth(2)
         h_control=_file0.Get("h_MWW_5_up")
         h_control_notPPS=_file0.Get("h_MWW_extra_tracks_5_up_notPPS")
         ratio=h_control.GetEntries()/h_control_notPPS.GetEntries()
@@ -61,7 +77,7 @@ def main(direc,Ycut,channel,background_method):
 
 
     if background_method=="template":
-        deno=h_MWW_extra_tracks_5_15_notPPS.GetEntries()
+        deno=_file0.Get("h_MWW_extra_tracks_5_15_notPPS").GetEntries()
         h1=_file0.Get("h_MWW_MX_control_notPPS")
         if Ycut=="Ycut": h1=_file0.Get("h_MWW_MX_control_Ycut_notPPS")
         h1.Scale(num/deno)
@@ -82,6 +98,38 @@ def main(direc,Ycut,channel,background_method):
         print "Data total prediction: ",h1.Integral(0,1001) 
         h1.SetMaximum(maximum)
         h1.Draw("e")
+
+    if background_method=="template_5up":
+        deno=_file0.Get("h_MWW_extra_tracks_5_up_notPPS").GetEntries()
+        h1=_file0.Get("h_MWW_MX_5_up_notPPS")
+        if Ycut=="Ycut": h1=_file0.Get("h_MWW_MX_5_up_Ycut_notPPS")
+        h1.Scale(num/deno)
+        print "Scale factor for high to low extra tracks for template_5up is: ",scale_factor
+        h_control_temp=_file0.Get("h_MWW_5_up")
+        #h_control_notPPS_temp=_file0.Get("h_MX_control_notPPS")
+        h_control_notPPS_temp=_file0.Get("h_MWW_extra_tracks_5_up_notPPS")
+        ratio=h_control_temp.GetEntries()/h_control_notPPS_temp.GetEntries()
+        ratioMC=h_control_temp.GetEntries()/(h_control_temp.GetEntries()+h_control_notPPS_temp.GetEntries())
+        print "Ratio: ",ratio
+
+        h1.Scale(ratio)
+        h1.Rebin(rebin)
+        h1.Sumw2()
+        h1.SetStats(0)
+        h1.GetYaxis().SetRangeUser(0,10)
+        h1.GetXaxis().SetTitle("MWW/MX")
+        h1.GetYaxis().SetTitle("Events")
+        print "Data total prediction: ",h1.Integral(0,1001) 
+        h1.SetMaximum(maximum)
+        h1.Draw("e")
+
+
+    #h0.Draw()
+    #h1.Draw("same")
+    #sys.exit(1)
+    h1.SetLineColor(1)
+    h1.SetLineWidth(2)
+    h1.SetMarkerStyle(22)
 
     #######################################################################################
     #Make Signal plots
@@ -142,7 +190,7 @@ def main(direc,Ycut,channel,background_method):
     #Make Background plots
     #######################################################################################
 
-    MCsamples=[#"WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8",#"WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8",#"WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8",
+    MCsamples=[#"WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8","WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8","WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8",
         "W1JetsToLNu_LHEWpT_100-150_TuneCP5_13TeV-amcnloFXFX-pythia8", "W1JetsToLNu_LHEWpT_150-250_TuneCP5_13TeV-amcnloFXFX-pythia8","W1JetsToLNu_LHEWpT_250-400_TuneCP5_13TeV-amcnloFXFX-pythia8","W1JetsToLNu_LHEWpT_400-inf_TuneCP5_13TeV-amcnloFXFX-pythia8","W2JetsToLNu_LHEWpT_100-150_TuneCP5_13TeV-amcnloFXFX-pythia8","W2JetsToLNu_LHEWpT_150-250_TuneCP5_13TeV-amcnloFXFX-pythia8","W2JetsToLNu_LHEWpT_250-400_TuneCP5_13TeV-amcnloFXFX-pythia8","W2JetsToLNu_LHEWpT_400-inf_TuneCP5_13TeV-amcnloFXFX-pythia8",
         #"DYJetsToLL_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8",#"DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8",#"DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8",
         "TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8",
@@ -167,69 +215,96 @@ def main(direc,Ycut,channel,background_method):
         "ZZ_TuneCP5_13TeV-pythia8"
     ]
 
-
     leg=TLegend(0.61,0.402,0.81,0.892)
     leg.SetFillColor(0)
     leg.SetLineColor(0)
-    #leg.SetTextSize(0.035)
     leg.SetTextSize(0.022)
     leg.SetTextFont(42)
+    leg.AddEntry(h0,"ABCD prediction","lep")
+    leg.AddEntry(h1,"Template prediction","lep")
 
-    leg.AddEntry(h1,"Data Background prediction","lep")
+    if backgroundMC:
+        histo_name="h_MWW_MX_0_4_tracks_100events"
+        if Ycut=="Ycut":     histo_name="h_MWW_MX_0_4_tracks_100events_Ycut"
+        hstack=THStack()
+        hMC=[]
+        fMC=[]
+        it=0
+        u=histo_name
+        digits=False
+        apply_extra_tracks_weight=True
+        if channel=="muon":
+            extra_tracks_weight=2.2*ratioMC
+        if channel=="electron":
+            extra_tracks_weight=1.6835*ratioMC
+        for sample in MCsamples:
+            fMC.append(TFile(directory+"/"+sample+".root"))
+            fMC[it].cd()
+            #This is if specified a range of histos
+            if digits:
+                hMC.append(fMC[it].Get(histo_list[i-1]))
+            else: hMC.append(fMC[it].Get(u))
+            ModifyHisto(hMC[it],sample,directory)
+            if apply_extra_tracks_weight:
+                hMC[it].Scale(extra_tracks_weight)
+            hMC[it].Rebin(rebin)
+            hstack.Add(hMC[it])
+            it=it+1
 
-    histo_name="h_MWW_MX_0_4_tracks_100events"
-    if Ycut=="Ycut":     histo_name="h_MWW_MX_0_4_tracks_100events_Ycut"
-    hstack=THStack()
-    hMC=[]
-    fMC=[]
-    it=0
-    u=histo_name
-    digits=False
-    apply_extra_tracks_weight=True
-    if channel=="muon":
-        extra_tracks_weight=2.2*ratioMC
-    if channel=="electron":
-        extra_tracks_weight=1.6835*ratioMC
-    for sample in MCsamples:
-        fMC.append(TFile(directory+"/"+sample+".root"))
-        fMC[it].cd()
-        #This is if specified a range of histos
-        if digits:
-            hMC.append(fMC[it].Get(histo_list[i-1]))
-        else: hMC.append(fMC[it].Get(u))
-        ModifyHisto(hMC[it],sample,directory)
-        if apply_extra_tracks_weight:
-            hMC[it].Scale(extra_tracks_weight)
-        hMC[it].Rebin(rebin)
-        hstack.Add(hMC[it])
-        it=it+1
+        leg.AddEntry(h2,legend_name("ExclusiveWW_a0w1e-6-SingleLepton-2017"),"l")
+        leg.AddEntry(h4,legend_name("ExclusiveWW_a0w2e-6-SingleLepton-2017"),"l")
+        leg.AddEntry(h5,legend_name("ExclusiveWW_a0w5e-6-SingleLepton-2017"),"l")
+        leg.AddEntry(h3,legend_name("ExclusiveWW_SM_FPMC-SingleLepton-2017"),"l")
 
-    leg.AddEntry(h2,legend_name("ExclusiveWW_a0w1e-6-SingleLepton-2017"),"l")
-    leg.AddEntry(h4,legend_name("ExclusiveWW_a0w2e-6-SingleLepton-2017"),"l")
-    leg.AddEntry(h5,legend_name("ExclusiveWW_a0w5e-6-SingleLepton-2017"),"l")
-    leg.AddEntry(h3,legend_name("ExclusiveWW_SM_FPMC-SingleLepton-2017"),"l")
-
-    len_a=len(hMC)
-    for sample in reversed(MCsamples):
-        if sample =="WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8" or sample=="WW_TuneCP5_13TeV-pythia8" or sample=="TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8" or sample =="QCD_Pt_170to300_TuneCP5_13TeV_pythia8" or sample=="WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" or sample =="TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8" or sample=="WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample == "W2JetsToLNu_LHEWpT_400-inf_TuneCP5_13TeV-amcnloFXFX-pythia8" or sample =="ExclusiveWW_SM_FPMC-SingleLepton-2017":
-            leg.AddEntry(hMC[len_a-1],legend_name(sample),"f")
-        len_a=len_a-1
+        len_a=len(hMC)
+        for sample in reversed(MCsamples):
+            if sample =="WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8" or sample=="WW_TuneCP5_13TeV-pythia8" or sample=="TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8" or sample =="QCD_Pt_170to300_TuneCP5_13TeV_pythia8" or sample=="WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" or sample =="TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8" or sample=="WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample =="DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8" or sample == "W2JetsToLNu_LHEWpT_400-inf_TuneCP5_13TeV-amcnloFXFX-pythia8" or sample =="ExclusiveWW_SM_FPMC-SingleLepton-2017":
+                leg.AddEntry(hMC[len_a-1],legend_name(sample),"f")
+            len_a=len_a-1
 
 
-    hstack.SetMaximum(maximum)
-    hstack.Draw("hist same")
-    h_mc_errors=hstack.GetStack().Last().Clone()
-    h_mc_errors.SetLineColor(1)
-    h_mc_errors.SetFillStyle(2)
-    h_mc_errors.SetFillColor(1)
-    h_mc_errors.SetFillStyle(3005)
-    h_mc_errors.Draw("e2 same")
+        hstack.SetMaximum(maximum)
+        hstack.Draw("hist same")
+        h_mc_errors=hstack.GetStack().Last().Clone()
+        h_mc_errors.SetLineColor(1)
+        h_mc_errors.SetFillStyle(2)
+        h_mc_errors.SetFillColor(1)
+        h_mc_errors.SetFillStyle(3005)
+        h_mc_errors.Draw("e2 same")
 
+    h0.Draw("esame")
     h1.Draw("esame")
     h2.Draw("histsame")
     h3.Draw("histsame")
     h4.Draw("histsame")
     h5.Draw("histsame")
+
+    diff_up=h1.Clone()
+    diff_down=h1.Clone()
+    bins_x=h0.GetNbinsX()
+    for i in range(0,bins_x+1):
+        h0_bc=h0.GetBinContent(i)
+        h1_bc=h1.GetBinContent(i)
+        diff=abs(h0_bc-h1_bc)
+        print diff
+        diff_up.SetBinContent(i,h1_bc+diff)
+        diff_down.SetBinContent(i,h1_bc-diff)
+
+    fout.cd()
+    h1.Write("background")
+    diff_up.Write("background_alphaUp")
+    diff_down.Write("background_alphaDown")
+    #h1.Scale(2)
+    data_obs=h1.Clone()
+    data_obs.Add(h1,h3)
+    data_obs.Write("data_obs")
+    #h1.Write("data_obs")
+    h2.Write("signal_1e6")
+    h3.Write("background_ExclWW")
+    h4.Write("signal_2e6")
+    h5.Write("signal_5e6")
+    fout.Write()
+    fout.Close()
 
     leg.Draw("same")
     latex=TLatex()
@@ -237,12 +312,16 @@ def main(direc,Ycut,channel,background_method):
     latex.SetTextSize(0.04)
     latex.SetTextAlign(11)
     #latex.DrawLatex(0.12,0.96,"CMS")
+    ycut_str=""
+    if Ycut=="Ycut":
+        ycut_str="_ycut"
     if channel=="muon":
         latex.DrawLatex(0.17,0.80,"{0}".format(directory[11:]))
     if channel=="electron":
         latex.DrawLatex(0.17,0.80,"{0}".format(directory[11:]))
-    c.Print("BackgroundPrediction_{0}_{1}.pdf".format(directory[11:],background_method))
+    c.Print("BackgroundPrediction_{0}_{1}{2}.pdf".format(directory[11:],background_method,ycut_str))
 
 if __name__=="__main__":
     #main(sys.argv[1],"h_MWW_MX_0_4_tracks","h_MWW_MX_5_up","muon")
+    #if len(sys.argv)==5:
     main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
